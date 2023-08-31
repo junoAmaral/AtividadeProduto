df4fc0024e40deb3e70f25c6f6e76b9c609dbdb5
juno.aferreira <juno.aferreira@CAR0588295W10-1>
2023-08-31 14:37:14 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadeProduto/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeProduto
+{
+    internal class Produto
+    {
+        //Atributos
+        public string nome;
+        public string descricao;
+        public int quantidade;
+        public double preço;
+        public bool status;
+
+        //Métodos
+        public void mostra()
+        {
+            Console.WriteLine($"{nome}\n {descricao}\n {quantidade} unidades em estoque\n R$ {preço}\n Status: {status}\n");
+        }
+
+        public void desativa()
+        {
+            quantidade = 0;
+            status = false;
+        }
+    }
+}
---
+++ AtividadeProduto/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using AtividadeProduto;
+
+Console.WriteLine("Olá, boas-vindas!\n");
+
+Produto bolacha = new Produto();
+bolacha.nome = "Bolacha de chocolate";
+bolacha.descricao = "Bolacha = biscoito + recheio + biscoito";
+bolacha.quantidade = 10;
+bolacha.preço = 03.40;
+bolacha.status = true;
+
+Produto biscoito = new Produto(); 
+biscoito.nome = "Bisoito de polvilho";
+biscoito.descricao = "Biscoito não tem recheio";
+biscoito.quantidade = 8;
+biscoito.preço = 07.10;
+biscoito.status = true;
+
+Produto cookie = new Produto();
+cookie.nome = "Cookie com gotas de chocolate";
+cookie.descricao = "Cookie tem recheio";
+cookie.quantidade = 12;
+cookie.preço = 04.80;
+cookie.status = true;
+
+bolacha.mostra();
+biscoito.mostra();
+
+cookie.desativa();
+cookie.mostra();
+
+Console.ReadKey();
